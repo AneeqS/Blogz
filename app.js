55dd4abc4281f969a46efc6353c988f79ade8486
AneeqS <aneeqshah27@gmail.com>
2019-02-10 14:09:08 -0500

    added configurations

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+//Variables
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     ejs = require("ejs"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer");
+
+//APP CONFIGURATIONS
+mongoose.connect("mongodb://localhost:27017/blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
